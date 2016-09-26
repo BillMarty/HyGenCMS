521acde6949c588e48d27bce22a4f55d52d611a5
BillMarty <bmarty@planetarypower.com>
2016-09-26 10:17:17 -0700

    Slight modification to logger calls.

--- hygencms/adc.py
+++ hygencms/adc.py
@@ -89,15 +89,11 @@ def setup():
                 break
 
         if path_success:
-            #logger.debug('ADC path_success at delay = ' + str(delay))
-            print('ADC path_success at delay = ' + str(delay))
-            try:
-                logger.debug('ADC path_success at delay = ' + str(delay))
-            except:
-                #ignore
+            logger.info('ADC path_success at delay = ' + str(delay))
+            #print('ADC path_success at delay = ' + str(delay))
         else:
-            #logger.debug('ADC path failed!')
-            print('ADC path failed!')
+            logger.warning('ADC path failed!')
+            #print('ADC path failed!')
             adc_setup = False
             return adc_setup
 
