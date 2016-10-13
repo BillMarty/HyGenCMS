f9545c984b2c7a42ce1087d995a7693e4001bb0a
BillMarty <bmarty@planetarypower.com>
2016-10-13 15:25:24 -0700

    Oops - also needed to remove powerdown from __main__.py.

--- hygencms/__main__.py
+++ hygencms/__main__.py
@@ -34,9 +34,6 @@ def main():
         '-w', '--watchdog', action='store_const', dest='watchdog', const=True,
         default=False, help='run the logger with a watchdog timer')
     parser.add_argument(
-        '-p', '--poweroff', action='store_const', dest='poweroff', const=True,
-        default=False, help='Trigger poweroff on GPIO P8_19 input')
-    parser.add_argument(
         '-t', '--time', action='store_const', dest='time', const=True,
         default=False, help='Set system time from DeepSea')
     args = parser.parse_args()
@@ -98,11 +95,9 @@ def main():
                               signal.SIGTSTP: 'terminate',  # suspend - configurable
                               }
         with context:
-            main_entry(config, handlers, daemon=True, watchdog=args.watchdog,
-                       power_off_enabled=args.poweroff, time_from_deepsea=args.time)
+            main_entry(config, handlers, daemon=True, watchdog=args.watchdog, time_from_deepsea=args.time)
     else:
-        main_entry(config, handlers, daemon=False, watchdog=args.watchdog,
-                   power_off_enabled=args.poweroff, time_from_deepsea=args.time)
+        main_entry(config, handlers, daemon=False, watchdog=args.watchdog, time_from_deepsea=args.time)
 
 
 if __name__ == '__main__':
