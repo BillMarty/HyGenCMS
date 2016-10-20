983609444a0d7cf3664c591a0a91b4b3f1218052
BillMarty <bmarty@planetarypower.com>
2016-10-20 15:58:05 -0700

    Corrected Matthew's quickie tuning.py.

--- hygencms/tuning.py
+++ hygencms/tuning.py
@@ -1,5 +1,10 @@
+# Woodward configuration - pulled out from config.py to make PID tuning updates easier.
 {
-	'Kp': 0.0,
-	'Ki': 0.5,
-	'Kd': 0.0,
+    'pin': pins.WW_PWM,
+    'Kp': 0.0,
+    'Ki': 0.5,
+    'Kd': 0.0,
+    'slew': 25,  # In percent per second max change
+    'setpoint': 25.0,  # Amps
+    'period': 1.0
 }
