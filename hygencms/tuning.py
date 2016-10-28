bacb69aa4435219b141197fe6597933ec95fb179
root <root@beaglebone.localdomain>
2016-10-28 22:25:21 +0000

    PID tuning parameters that make Kurt happy :-)

--- hygencms/tuning.py
+++ hygencms/tuning.py
@@ -2,8 +2,8 @@
 {
     #The 'pin' value is set at boot and has no need to change later, so no reason for it to live in this file.
     #'pin': pins.WW_PWM,
-    'Kp': 0.0,
-    'Ki': 0.5,
+    'Kp': 0.2,
+    'Ki': 0.4,
     'Kd': 0.0,
     'slew': 25,  # In percent per second max change
     'setpoint': 25.0,  # Amps
