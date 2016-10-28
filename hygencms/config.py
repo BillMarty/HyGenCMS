b2848b6fb2fd697340b48f661b0c495398815717
BillMarty <bmarty@planetarypower.com>
2016-10-28 15:30:10 -0700

    Catching config.py up to KA's changes in tuning.py.

--- hygencms/config.py
+++ hygencms/config.py
@@ -43,8 +43,8 @@ defaults = {
     # Control signal to Woodward
     'woodward': {
         'pin': pins.WW_PWM,
-        'Kp': 0.0,
-        'Ki': 0.5,
+        'Kp': 0.2,
+        'Ki': 0.4,
         'Kd': 0.0,
         'slew': 25,  # In percent per second max change
         'setpoint': 25.0,  # Amps
