096a9cb419b1f8d5662866d71de99fee29bf9348
BillMarty <bmarty@planetarypower.com>
2017-01-19 11:49:37 -0800

    Debugging, plus code inspection fixes.

--- hygencms/powerclient.py
+++ hygencms/powerclient.py
@@ -5,15 +5,13 @@
 
 import sys
 import time
-import logging
-from monotonic import monotonic
-from . import pins
 from .asyncio import AsyncIOThread
 from .deepseaclient import DeepSeaClient
 
 # TODO Update setpoint in config.py and tuning.py.
 # TODO We're not using an_300v_volt, so let's comment it out.
 # TODO Add my calculated power to the run log file.
+
 
 class PowerClient(AsyncIOThread):
     """
@@ -38,34 +36,34 @@ class PowerClient(AsyncIOThread):
         self.new_log_file = False
         # Input items:
         #   current measurement
-        analog_dict = config["analog"]
-        analog_measurements = analog_dict["measurements"]
+        analog_dict = config['analog']
+        analog_measurements = analog_dict['measurements']
         self.analog_current = analog_measurements[0]
         # Debug
         print('self.analog_current: {}'.format(self.analog_current))
         #   voltage measurement
-        deepsea_dict = config["deepsea"]
-        measurement_file = deepsea_dict["mlistfile"]
+        deepsea_dict = config['deepsea']
+        measurement_file = deepsea_dict['mlistfile']
         deepsea_measurement_list = \
             DeepSeaClient.read_measurement_description(measurement_file)
         for m in deepsea_measurement_list:
-            if m[PowerClient.NAME] == "300V Bus Voltage":
+            if m[PowerClient.NAME] == '300V Bus Voltage':
                 self.hibus_voltage = m
         if not self.hibus_voltage:
-            self._logger.info("!!PowerClient failed to read voltage"
-                              "measurement info!!")
+            self._logger.info('!!PowerClient failed to read voltage'
+                              'measurement info!!')
         # Debug
         print('self.hibus_voltage: {}'.format(self.hibus_voltage))
         # Initialize our output variable in the data store.
-        self.data_store["calc_300v_pwr"] = 0.0
+        self.data_store['calc_300v_pwr'] = 0.0
         # Debug - write the voltage and current values that we're reading so
         #   I know the reading is working.
-        self.data_store["pwr.voltage"] = 0.0
-        self.data_store["pwr.current"] = 0.0
+        self.data_store['pwr.voltage'] = 0.0
+        self.data_store['pwr.current'] = 0.0
         # Debug
         self.first_pass = True
         # Log to info that we've started
-        self._logger.info("Started PowerClient")
+        self._logger.info('Started PowerClient')
 
     def run(self):
         """
@@ -96,9 +94,9 @@ class PowerClient(AsyncIOThread):
             # In early passes, voltage and current may be None types, until
             #   readings come in.  Prevent the exception...
             if voltage and current:
-                self.data_store["calc_300v_pwr"] = voltage * current
-                self.data_store["pwr:voltage"] = voltage
-                self.data_store["pwr:current"] = current
+                self.data_store['calc_300v_pwr'] = voltage * current
+                self.data_store['pwr.voltage'] = voltage
+                self.data_store['pwr.current'] = current
         time.sleep(0.25)
 
     def print_data(self):
@@ -106,18 +104,18 @@ class PowerClient(AsyncIOThread):
         Print PowerClient's data of interest.
         """
         # Calculated power.
-        display = "%20s %10.2f %10s" % ("calc_300v_pwr",
-                                        self.data_store["calc_300v_pwr"],
-                                        "kW")
+        display = '%20s %10.2f %10s' % ('calc_300v_pwr',
+                                        self.data_store['calc_300v_pwr'],
+                                        'kW')
         print(display)
         # Input variables
-        display = "%20s %10.2f %10s" % ("pwr.voltage",
-                                        self.data_store["pwr.voltage"],
-                                        "V")
+        display = '%20s %10.2f %10s' % ('pwr.voltage',
+                                        self.data_store['pwr.voltage'],
+                                        'V')
         print(display)
-        display = "%20s %10.2f %10s" % ("pwr.current",
-                                        self.data_store["pwr.current"],
-                                        "A")
+        display = '%20s %10.2f %10s' % ('pwr.current',
+                                        self.data_store['pwr.current'],
+                                        'A')
         print(display)
 
     def csv_header(self):
